fc31f14621cb56ba76e868cdaf0b5d3b12651bc9
Darla Sawler <darlasawler@gmail.com>
2013-12-31 16:42:18 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# /public
+
+My public self.
+
+Feel free to fork this and make it your own public self.
+
---
+++ buster benson/tweets/data/js/payload_details.js
@@ -0,0 +1,5 @@
+var payload_details =  {
+  "tweets" : 14551,
+  "created_at" : "Wed Jul 17 18:03:25 +0000 2013",
+  "lang" : "en"
+}
